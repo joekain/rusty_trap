8296d6fc8b271e995d6ca28fe773853ef1d0b587
Joseph Kain <joekain@gmail.com>
2015-06-27 11:52:53 -0700

    exec and continue API
    This API is based on test_exec and allows it to compile properly.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-#[test]
-fn it_works() {
+use std::ffi::CString;
+
+pub type TrapInferior = i64;
+
+pub fn trap_inferior_exec(filename: &str, args: &[&str]) -> TrapInferior {
+    return 0;
+}
+
+pub fn trap_inferior_continue(inferior: TrapInferior) -> i8 {
+    return 0;
 }
