dc323e6cb3886d6387cfc827b279a591261c1701
Joseph Kain <joekain@gmail.com>
2015-06-27 11:52:38 -0700

    Use legal syntax

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,6 +2,6 @@ extern crate rusty_trap;
 
 #[test]
 fn it_can_exec () {
-    let inferior = rusty_trap::trap_inferior_exec("./inferiors/twelve", { 0 });
+    let inferior = rusty_trap::trap_inferior_exec("./inferiors/twelve", &[]);
     assert_eq!(12, rusty_trap::trap_inferior_continue(inferior));
 }
