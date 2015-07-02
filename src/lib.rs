eb313ee449fe539206fe164d1594615dbe3edb76
Joseph Kain <joekain@gmail.com>
2015-07-01 21:05:06 -0700

    Implement trap_inferior_exec and trap_inferior_continue

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,60 @@
+extern crate nix;
+extern crate libc;
+
+use nix::unistd::*;
+use nix::unistd::Fork::*;
+use libc::pid_t;
+use nix::Error;
+use nix::errno;
+use nix::sys::ptrace::*;
+use nix::sys::ptrace::ptrace::*;
+use nix::sys::wait::*;
 use std::ffi::CString;
+use std::ptr;
+use nix::sys::signal;
 
-pub type TrapInferior = i64;
+pub type TrapInferior = pid_t;
 
-pub fn trap_inferior_exec(filename: &str, args: &[&str]) -> TrapInferior {
-    return 0;
+fn exec_inferior(filename: &str, args: &[&str]) -> () {
+    let c_filename = &CString::new(filename).unwrap();
+    ptrace(PTRACE_TRACEME, 0, ptr::null_mut(), ptr::null_mut())
+        .ok()
+        .expect("Failed PTRACE_TRACEME");
+    execve(c_filename, &[], &[])
+        .ok()
+        .expect("Failed execve");
+    unreachable!();
+}
+
+fn attach_inferior(pid: pid_t) -> Result<TrapInferior, Error> {
+    match waitpid(pid, None) {
+        Ok(WaitStatus::Stopped(pid, signal::SIGTRAP)) => return Ok(pid),
+        Ok(_) => panic!("Unexpected stop in attach_inferior"),
+        Err(e) => return Err(e)
+    }
+}
+
+pub fn trap_inferior_exec(filename: &str, args: &[&str]) -> Result<TrapInferior, Error> {
+    loop {
+        match fork() {
+            Ok(Child)                      => exec_inferior(filename, args),
+            Ok(Parent(pid))                => return attach_inferior(pid),
+            Err(Error::Sys(errno::EAGAIN)) => continue,
+            Err(e)                         => return Err(e)
+        }
+    }
 }
 
 pub fn trap_inferior_continue(inferior: TrapInferior) -> i8 {
-    return 0;
+    let pid = inferior;
+
+    ptrace(PTRACE_CONT, pid, ptr::null_mut(), ptr::null_mut())
+        .ok()
+        .expect("Failed PTRACE_CONTINUE");
+
+    match waitpid(pid, None) {
+        Ok(WaitStatus::Exited(_pid, code)) => return code,
+        Ok(_) => panic!("Unexpected stop in trap_inferior_continue"),
+        Err(_) => panic!("Unhandled error in trap_inferior_continue")
+    }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,6 +2,6 @@ extern crate rusty_trap;
 
 #[test]
 fn it_can_exec () {
-    let inferior = rusty_trap::trap_inferior_exec("./inferiors/twelve", &[]);
+    let inferior = rusty_trap::trap_inferior_exec("./target/debug/twelve", &[]).unwrap();
     assert_eq!(12, rusty_trap::trap_inferior_continue(inferior));
 }
