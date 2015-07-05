7c091c9595e87dbabb5ecbbfc7bdf537ec04e9ea
Joseph Kain <joekain@gmail.com>
2015-07-04 20:40:09 -0700

    Breakpoint test

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,3 +7,22 @@ fn it_can_exec () {
         .unwrap();
     assert_eq!(12, rusty_trap::trap_inferior_continue(inferior));
 }
+
+#[test]
+fn it_can_set_breakpoints () {
+    let mut bp: rusty_trap::Breakpoint = 0;
+    let mut breakpoint_count: i32 = 0;
+    let mut inferior = 0;
+
+    rusty_trap::trap_set_breakpoint_callback(|passed_inferior, passed_bp| {
+        assert_eq!(passed_inferior, inferior);
+        assert_eq!(passed_bp, bp);
+        breakpoint_count += 1;
+    }).unwrap();
+
+    let inferior = rusty_trap::trap_inferior_exec("./target/debug/twelve", &[]).unwrap();
+    bp = rusty_trap::trap_inferior_set_breakpoint(inferior, "main");
+    trap_inferior_continue(inferior);
+
+    assert_eq!(breakpoint_count, 1);
+}
