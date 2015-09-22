bc39d9228103aa139590ae8c6f89a8097bab9a8c
Joseph Kain <joekain@gmail.com>
2015-09-22 14:00:58 -0700

    Test multiple distinct breakpoints

--- tests/lib.rs
+++ tests/lib.rs
@@ -39,3 +39,27 @@ fn it_can_handle_a_breakpoint_more_than_once () {
 
     assert_eq!(breakpoint_count, 5);
 }
+
+#[test]
+fn it_can_handle_multiple_breakpoints () {
+    let mut foo_count:  i32 = 0;
+    let mut main_count: i32 = 0;
+
+    let inferior = rusty_trap::trap_inferior_exec(Path::new("./target/debug/loop"), &[])
+        .unwrap();
+    let bp_foo  = rusty_trap::trap_inferior_set_breakpoint(inferior, 0x5555555587b0);
+    let bp_main = rusty_trap::trap_inferior_set_breakpoint(inferior, 0x555555558bd0);
+    rusty_trap::trap_inferior_continue(inferior, &mut |passed_inferior, bp_passed| {
+        assert_eq!(passed_inferior, inferior);
+        if bp_passed == bp_foo {
+            foo_count  += 1
+        } else if bp_passed == bp_main {
+             main_count += 1
+        } else {
+            panic!("Unknown breakpoint {}", bp_passed)
+        }
+    });
+
+    assert_eq!(foo_count, 5);
+    assert_eq!(main_count, 5)
+}
